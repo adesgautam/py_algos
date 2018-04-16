0439d76dbf12a24f4a6e8611e519e078ca77986e
Adesh Gautam <adeshg7@gmail.com>
2018-04-16 11:49:02 +0530

    Add files via upload

---
+++ mergesort.py
@@ -0,0 +1,46 @@
+def merge(arr, l, m, r):
+
+    L = arr[:m]
+    R = arr[m:]
+    n1 = len(L)
+    n2 = len(R)
+ 
+    i, j, k = 0, 0, 0    
+ 
+    while i < n1 and j < n2 :
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+ 
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+def mergeSort(arr,l,r):
+    if l < r:
+        # Same as (l+r)/2, but avoids overflow for
+        # large l and h
+        m = (l+(r-1))/2
+ 
+        # Sort first and second halves
+        mergeSort(arr, l, m)
+        mergeSort(arr, m+1, r)
+        merge(arr, l, m, r)
+ 
+arr = [12, 11, 13, 5, 6, 7]
+n = len(arr)
+print("Given array is: {0}".format(arr))
+ 
+mergeSort(arr,0,n)
+print("Sorted array is: {0}".format(arr))
+
---
+++ quicksort.py
@@ -0,0 +1,23 @@
+# Adesh Gautam
+def quicksort(arr, low, high):
+	if low<high:
+		pi = partition(arr, low, high)
+		quicksort(arr, low, pi-1)
+		quicksort(arr, pi+1, high)
+
+
+def partition(arr, low , high):
+	pivot = arr[high]
+	i=low-1
+
+	for j in range(low, high):
+		if arr[j]<=pivot:
+			i+=1
+			arr[i], arr[j] = arr[j], arr[i]
+
+	arr[high], arr[i+1] = arr[i+1], arr[high]
+	return i+1
+
+arr = [3,5,8,2,5,9,3,10]
+quicksort(arr, 0, 7)
+print(arr)
