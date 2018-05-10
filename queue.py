8ac80047930f025437e657ca36801d74b8b87ed7
Adesh Gautam <adeshg7@gmail.com>
2018-05-10 12:52:38 +0530

    queue.py

--- queue.py
+++ queue.py
@@ -2,19 +2,19 @@
 class Queue:
 	def __init__(self):
 		self.items = []
-		self.head = -1
-		self.tail = -1
+		self.head = 0
+		self.tail = 0
 
 	def enqueue(self, x):
-		self.tail+=1
 		self.items.insert(self.tail, x)
+		self.tail+=1
 
 	def dequeue(self):
 		if self.isEmpty():
 			print("Queue is empty !!!")
 		else:
-			self.items.remove(self.items[self.head])
-			self.head-=1
+			print(self.head)
+			self.items.pop(self.head)
 
 	def isEmpty(self):
 		return self.head == self.tail
@@ -24,13 +24,24 @@ class Queue:
 
 q = Queue()
 print(q.isEmpty())
+# insert
 for i in range(1,6):
 	q.enqueue(i)
+
+# check
 print(q.items, q.head, q.tail)
 print(q.size())
 print(q.isEmpty())
+
+# remove
 q.dequeue()
-print(q.items)
+# check
+print(q.items, q.head, q.tail)
+
+# remove
+q.dequeue()
+# check
+print(q.items, q.head, q.tail)
 	
 
 
