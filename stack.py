5634eb5ad8461baf30c0853ae2adbcee5deda5b2
Adesh Gautam <adeshg7@gmail.com>
2018-05-10 11:19:30 +0530

    added stack.py

--- stack.py
+++ stack.py
@@ -2,23 +2,31 @@
 class Stack:
 	def __init__(self):
 		self.items = []
+		self.head = -1
 
 	def push(self, x):
-		self.items.append(x)
+		self.head+=1
+		self.items.insert(self.head, x)
 
 	def pop(self):
-		self.items.pop()
+		if self.isEmpty():
+			print("Stack is empty !!!")
+		else:
+			# print(self.head)
+			self.items.pop(self.head)
+			head=-1
 
 	def size(self):
 		return len(self.items)
 
 	def isEmpty(self):
-		return self.items == []
+		return self.head == -1
 
 s = Stack()
+s.pop()
 for i in range(1,6):
 	s.push(i)
-print(s.items)
+print(s.items, s.head)
 s.pop()
 print(s.items)
 print(s.isEmpty())
