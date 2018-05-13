4484a86cf4d2b96ff9fcffd08cac8c8da3051f16
Adesh Gautam <adeshg7@gmail.com>
2018-05-13 22:14:52 +0530

    updated binary_search_tree.py

--- binary_search_tree.py
+++ binary_search_tree.py
@@ -31,8 +31,35 @@ def search_x(root, x):
 	else:
 		return search_x(root.right, x)
 
+def minnode(root):
+	current = root
+	while(current.left is not None):
+		current = current.left
+	return current
+
 def delete(root, x):
-	pass
+	if root is None:
+		return root
+	if root.val>x :
+		root.left = delete(root.left, x)
+	elif root.val<x :
+		root.right = delete(root.right, x)
+	else:
+		if root.left==None:     # 1 child
+			temp = root.right
+			root = None
+			return temp
+		elif root.right==None:  # 1 child
+			temp = root.left
+			root = None
+			return temp
+		
+		# 2 children
+		temp = minnode(root.right)
+		root.key = temp.key
+		root.right = delete(root.right, temp.key)
+
+	return root
 
 def inorder(root):
 	if root:
@@ -64,6 +91,10 @@ insert(root, Node(90))
 inorder(root)
 f = search_x(root,40)
 print('\n',f)
+
+f = delete(root, 20)
+print(f.val)
+inorder(root)
 
 
 
